138d88f701d72a79ab59e6c24a1dbc6503d23b09
arianedimarzio <dimarzioa@wit.edu>
2021-04-11 09:38:53 -0400

    Added the Step Counter using a sensor. Calculated distance travelled by using average stride length based on gender.

--- app/src/main/java/edu/wit/mobileapp/appdevproject/Steps.java
+++ app/src/main/java/edu/wit/mobileapp/appdevproject/Steps.java
@@ -2,7 +2,10 @@ package edu.wit.mobileapp.appdevproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobParameters;
+import android.app.job.JobService;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -12,13 +15,16 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Steps extends AppCompatActivity implements SensorEventListener{
+public class Steps extends AppCompatActivity {
 
     SensorManager sensorManager;
+    Sensor countSensor;
     TextView steps, distanceOutput;
     boolean running = false;
     double distance = 0.0;
     String gender;
+    int totalSteps = 0;
+    double MagnitudePrevious = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,48 +43,78 @@ public class Steps extends AppCompatActivity implements SensorEventListener{
         distanceOutput = findViewById(R.id.distanceOutput);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        SensorEventListener stepDetector = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (event != null) {
+                    float x_acceleration = event.values[0];
+                    float y_acceleration = event.values[1];
+                    float z_acceleration = event.values[2];
+
+                    double Magnitude = Math.sqrt(x_acceleration * x_acceleration + y_acceleration * y_acceleration + z_acceleration * z_acceleration);
+                    double MagnitudeDelta = Magnitude - MagnitudePrevious;
+                    MagnitudePrevious = Magnitude;
+
+                    if (MagnitudeDelta > 6) {
+                        totalSteps++;
+                    }
+                    steps.setText(String.valueOf(totalSteps));
+                    Log.v("myApp", "Sensor event: Step count: " + totalSteps);
+
+                    //calculation of stride length to determine distance based on
+                    // average gender stride length
+                    if (gender.equals("Male")) {
+                        distance = (totalSteps * 2.5) / 5280;
+                        Log.v("myApp", "Distance Male: " + distance);
+                    } else {
+                        distance = (totalSteps * 2.2) / 5280;
+                        Log.v("myApp", "Distance Female: " + distance);
+                    }
+                    distanceOutput.setText(distance + "miles");
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                Log.v("myApp", "Accuracy Changed: Sensor: " + sensor + " ; accuracy: " + accuracy);
+            }
+        };
+        sensorManager.registerListener(stepDetector, countSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        running = true;
-        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        if(countSensor != null) {
-            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
-        }
-        else {
-            Toast.makeText(this, "Sensor not found!", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
+    //saves steps to shared preferences so data is not erased when you close the app
     protected void onPause() {
         super.onPause();
-        running = false;
-        //if you unregister the hardware will stop detecting steps
-        //sensorManager.unregisterListener(this);
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+
+        editor.putInt("stepCount", totalSteps);
+        editor.apply();
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if(running) {
-            steps.setText(String.valueOf(event.values[0]));
-            //calculation of stride length to determine distance based on
-            // average gender stride length
-            if (gender.equals("Male")) {
-                distance = (event.values[0] * 2.5) / 5280;
-            } else {
-                distance = (event.values[0] * 2.2) / 5280;
-            }
-            distanceOutput.setText(distance + "@string/miles");
-        }
+    //saves steps to shared preferences so data is not erased when you close the app
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+
+        editor.putInt("stepCount", totalSteps);
+        editor.apply();
     }
 
-    //intentionally left blank
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        totalSteps = sharedPreferences.getInt("stepCount", 0);
     }
-
 }
+
